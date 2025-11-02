6b8ea8341f96d8a4b10d25fc6d217c5b8248e358
Sielanse <235517418+sielanse@users.noreply.github.com>
2025-11-02 10:08:28 +0100

    Fix basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/b4e-website',
   reactStrictMode: true,
   images: {
     unoptimized: true, // Required for static export
