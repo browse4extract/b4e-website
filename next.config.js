36407255143ec73d5c5d6587bb65abf6bebb86ee
Sielanse <235517418+sielanse@users.noreply.github.com>
2025-11-02 10:05:40 +0100

    web deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
+    unoptimized: true, // Required for static export
     remotePatterns: [
       {
         protocol: 'https',
